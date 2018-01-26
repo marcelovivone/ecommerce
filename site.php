ea20a26eac8383d45fac0ac5da106aaade1bdcc0
Marcelo Vivone <marcelovivon@gmail.com>
2018-01-26 15:31:16 -0200

    WEB Service Endereço

--- site.php
+++ site.php
@@ -141,21 +141,116 @@ $app->post("/cart/freight", function() {
 
 });
 
-// rota para a finalização da compra
+// rota para página da finalização da compra
 $app->get("/checkout", function() {
 
 	User::verifyLogin(false);
 
+	$address = new Address();
+
 	$cart = Cart::getFromSession();
 
-	$address = new Address();
+	// se o CEP já existir no carrinho: carrega em $_GET o CEP do carrinho
+	if (isset($_GET['zipcode'])) {
+
+		$_GET['zipcode'] = $cart->getdeszipcode();
+
+	}
+
+	// verifica se o cep foi informado
+	if (isset($_GET['zipcode'])){
+
+		// lê os dados de endereço do cep
+		$address->loadFromCEP($_GET['zipcode']);
+		
+		// atribui o novo cep ao carrinho
+		$cart->setdeszipcode($_GET['zipcode']);
+
+		// salva os novos dados no banco de dados
+		$cart->save();
+
+		// refaz o cálculo dos valores de frete
+		$cart->getCalculateTotal();
+
+	}
+
+	// define os dados no carrinho, mesmo que com conteúdos vazios
+	if (!$address->getdesaddress()) $address->setdesaddress('');
+	if (!$address->getdescomplement()) $address->setdescomplement('');
+	if (!$address->getdesdistrict()) $address->setdesdistrict('');
+	if (!$address->getdescity()) $address->setdescity('');
+	if (!$address->getdesstate()) $address->setdesstate('');
+	if (!$address->getdescountry()) $address->setdescountry('');
+	if (!$address->getdeszipcode()) $address->setdeszipcode('');
 
 	$page = new Page();
 
 	$page->setTpl("checkout", [
 		'cart'=>$cart->getValues(),
-		'address'=>$address->getValues()
+		'address'=>$address->getValues(),
+		'products'=>$cart->getProducts(),
+		'error'=>$address::getMsgError()
 	]);
+
+});
+
+// rota para salvar dados
+$app->post("/checkout", function() {
+
+	User::verifyLogin(false);
+
+	// valida o preenchimento dos campos do formulário
+	if (!isset($_POST['zipcode']) || $_POST['zipcode'] === ''){
+		Address::setMsgError('Informe o CEP.');
+		header("Location: /checkout");
+		exit;
+	}
+
+	if (!isset($_POST['desaddress']) || $_POST['desaddress'] === ''){
+		Address::setMsgError('Informe o endereço.');
+		header("Location: /checkout");
+		exit;
+	}
+
+	if (!isset($_POST['desdistrict']) || $_POST['desdistrict'] === ''){
+		Address::setMsgError('Informe o bairro.');
+		header("Location: /checkout");
+		exit;
+	}
+
+	if (!isset($_POST['descity']) || $_POST['descity'] === ''){
+		Address::setMsgError('Informe a cidade.');
+		header("Location: /checkout");
+		exit;
+	}
+
+	if (!isset($_POST['desstate']) || $_POST['desstate'] === ''){
+		Address::setMsgError('Informe o estado.');
+		header("Location: /checkout");
+		exit;
+	}
+
+	if (!isset($_POST['descountry']) || $_POST['descountry'] === ''){
+		Address::setMsgError('Informe o país.');
+		header("Location: /checout");
+		exit;
+	}
+
+	$user = User::getFromSession();
+	$user->get($user->getiduser());
+	$address = new Address();
+
+	// sobrescrevendo o zipcode porque o campo no formulário não está com o mesmo nome do banco
+	$_POST['deszipcode'] = $_POST['zipcode'];
+	
+	$_POST['idperson'] = $user->getidperson();
+
+	$address->setData($_POST);
+
+	$address->save();
+
+	header('Location: /order');
+	exit;
 
 });
 
